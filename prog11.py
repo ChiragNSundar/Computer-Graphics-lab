61efb38c3fb36380a0ee61b180e1e98ce3b40ce9
Chirag N Sundar <99790072+Hazard-Chirag@users.noreply.github.com>
2024-08-01 21:43:14 +0530

    Update prog11.py

--- prog11.py
+++ prog11.py
@@ -9,7 +9,7 @@ blurred=cv2.GaussianBlur(gray,(5,5),0)
 
 thresh=cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,15,4)
 
-contours,hierarchy=cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+contours,hierarchy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img,contours,-1,(0,255,0),2)
 imgrgb=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
